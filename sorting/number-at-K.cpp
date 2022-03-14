af63c8a36e33c15b4cf67db51c5a9730a581ff98
Sinae <sinae.shannen@gmail.com>
2022-03-15 00:43:24 +0900

    K번째수 문제풀이 리팩토링

--- sorting/number-at-K.cpp
+++ sorting/number-at-K.cpp
@@ -1,39 +1,32 @@
 /* 정렬 > K번째수 문제 첫번째 풀이
 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges */
 
-#include <string>
 #include <vector>
+#include <utility>
 
 using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
-
-    for (vector<int> command : commands)
+    
+    for (vector<int>& command : commands)
     {
-        vector<int> array_copied;
-        const int start = command[0], end = command[1], target = command[2];
-
-        for (auto index = start-1; index < end; index++)
+        // 간단하게 assign operator로 복사 (deep copy 지원, array 크기가 작음)
+        vector<int> array_sorted = array;
+        
+        const auto FIRST = command[0] - 1, LAST = command[1], TARGET = command[0] + command[2] - 2;
+        for (auto position = FIRST; position <= TARGET; position++)
         {
-            array_copied.push_back(array[index]);
-        }
-
-        for (auto position = 0UL; position < target; position++)
-        {
-            for (auto compare = position+1; compare < array_copied.size(); compare++)
+            for (auto compare = position + 1; compare < LAST; compare++)
             {
-                if (array_copied[position] > array_copied[compare])
-                {
-                    const int temp = array_copied[position];
-                    array_copied[position] = array_copied[compare];
-                    array_copied[compare] = temp;
-                }
+                if (array_sorted[position] > array_sorted[compare])
+                    // std library 활용
+                    swap(array_sorted[position], array_sorted[compare]);
             }
         }
-
-        answer.push_back(array_copied[target-1]);
+        
+        answer.push_back(array_sorted[TARGET]);
     }
-
+    
     return answer;
 }
