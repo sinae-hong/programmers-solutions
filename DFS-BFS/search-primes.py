29068fe137dc898cc8c3c6aa8d96161f1700358f
Sinae <sinae.shannen@gmail.com>
2022-03-30 22:48:41 +0900

    소수 찾기 문제풀이 최적화 인덱싱을 통해 소수 여부를 바로 확인하여 리스트 탐색 시간 제거

--- DFS-BFS/search-primes.py
+++ DFS-BFS/search-primes.py
@@ -1,26 +1,26 @@
 # 완전탐색 > 소수 찾기 문제풀이 (#13)
 # 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
 
-def primes_up_to(number):
-    sieve = [ False, False ] + [ True ] * (number-1)
-    for k in range(2, int(number**0.5) + 1):
+def sieve_for_primes(limit):
+    sieve = [ False, False ] + [ True ] * (limit-1)
+    for k in range(2, int(limit**0.5) + 1):
         if sieve[k]:
-            sieve[2*k::k] = [ False ] * (number//k - 1)
-    return [ k for (k, value) in enumerate(sieve) if value ]
+            sieve[2*k::k] = [ False ] * (limit//k - 1)
+    return sieve
 
-def possibility(current, numbers, primes):    
+def possibility(current, numbers, sieve):    
     result = 0
     if len(numbers) == 0: return 0
     for number in set(numbers):
         new = current + number
         if int(new) == 0: continue
-        if int(new) in primes: result += 1
+        if sieve[int(new)]: result += 1
         p = numbers.index(number)
         numbers_left = numbers[:p] + numbers[p+1:]
-        result += possibility(new, numbers_left, primes)
+        result += possibility(new, numbers_left, sieve)
     return result
 
 def solution(numbers):
-    primes = primes_up_to(int('9'*len(numbers)))
+    sieve = sieve_for_primes(int('9'*len(numbers)))
     number_list = [ n for n in numbers ]
-    return possibility('0', number_list, primes)
+    return possibility('0', number_list, sieve)
